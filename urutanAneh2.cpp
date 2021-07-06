1903208c80783420f679da8fe7322656d36993aa
suupuushan <syafiq.asyraf2001@gmail.com>
2021-07-06 11:25:05 +0700

    versi lain

--- urutanAneh2.cpp
+++ urutanAneh2.cpp
@@ -1,40 +1,21 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-// deklarasi fungsi reversed
+// deklarasi fungsi
 int reversed(int x);
+bool check(int a[], int n);
 
 int main(){
     int n;
     cin >> n; // input banyak bilangan
 
     int a[n]; // insialisasi array
-    string str;
-
-    getline(cin >> ws, str); // membaca input
-    stringstream s(str);
 
     // menambahkan input ke array
-    int i = 0;
-    while (!s.eof()){
-        s >> a[i];
-        i++;
-    }
-    
-    // pengecekan apakah ada nilai yang <= 0 atau >= 100
-    bool check = false;
-    for (int j = 0; j < n; j++){
-        if ((a[j] <= 0) || (a[j] >= 100)){
-            check = true;
-            break;
-        }
-    }
+    for (int i = 0; i<n; i++) cin >> a[i];
 
-    if (check){
+    if (check(a, n)){
         cout << "Input value exceeds limit!!!" << endl; // pesan error
     } else {
         // membalik semua nilai, ex 21 => 12
@@ -54,24 +35,35 @@ int main(){
             cout << a[j] << endl;
         }
     }
-
     cin.get();
     return 0;
 }
 
+//fungsi membalik angka
 int reversed(int x){
     if (x < 10){
         x = x*10;
         return x;
-    }
-    if (x % 10 == 0){
+    } else if (x % 10 == 0){
         x = x/10;
+        return x;
+    } else {
+        int rem, num = 0;
+        while (x != 0){
+            rem = x % 10;
+            num = num * 10 + rem;
+            x = x / 10;
+        }
+        return num;
     }
-    int rem, num = 0;
-    while (x != 0){
-        rem = x % 10;
-        num = num * 10 + rem;
-        x = x / 10;
+}
+
+// fungsi pengecekan apakah ada nilai yang <= 0 atau >= 100
+bool check(int a[], int n){
+    for (int i = 0; i < n; i++){
+        if (a[i] <= 0 || a[i] >= 100){
+            return true;
+        }
     }
-    return num;
+    return false;
 }
